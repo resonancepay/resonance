24d69d698f1343aceb6c23947d15f08f8f6cce91
Mark <feranmijogbodo@gmail.com>
2026-06-30 11:24:24 +0100

    fix: fixed staff portal issue

--- apps/staff-portal/src/app/(auth)/reset-password/page.tsx
+++ apps/staff-portal/src/app/(auth)/reset-password/page.tsx
@@ -1,13 +1,17 @@
 import { ResetPasswordScreen } from "@/features/auth/screens/reset-password.screen";
 import { Metadata } from "next";
-import React from "react";
+import React, { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Reset Password | Resonance Staff Portal",
 };
 
 function ResetPassword() {
-  return <ResetPasswordScreen />;
+  return (
+    <Suspense>
+      <ResetPasswordScreen />
+    </Suspense>
+  );
 }
 
 export default ResetPassword;
